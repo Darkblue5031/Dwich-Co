6cf575de9c8ed3d490f85c2cec9277b4b96aa756
kilecon <mathieu.barboteau@ynov.com>
2024-03-26 23:48:21 +0100

    add files

---
+++ insert.py
@@ -0,0 +1,17 @@
+# from food.models import burger
+
+# chiche_burger = burger(name="Le Chiche", pImage='le_chiche.png', 
+#                        description='Galette de Pois Chiche - Féta - Iceberg - Poivrons Marinés -Sauce Ranch',
+#                        price=8.50, is_online=True
+#                        )
+
+
+# class burger(models.Model):
+#     name = models.CharField(max_length=120)
+#     pImage = models.URLField()
+#     description = models.CharField(max_length=255)
+#     price = models.DecimalField(max_digits=4, decimal_places=2)
+#     is_online = models.BooleanField(default = False)
+#     is_special = models.BooleanField(default = False)
+
+# ('Le CHICHE', 'Pois Chiche - Poivrons Marinés - Cheddar - Sauce Ranch', 8.50, FALSE, 8.50, TRUE, FALSE);
---
+++ memo.cmd
@@ -0,0 +1,6 @@
+venv\scripts\activate
+
+
+Chef_Julien
+chefjulien@dev.fr
+Dwitch&Co_Admin
